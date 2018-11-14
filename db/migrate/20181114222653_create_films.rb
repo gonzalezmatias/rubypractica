7c053487799e8ffc3a15bedda2f92be87854bda2
Matias E Gonzalez <gonzalez.matias.ok@gmail.com>
2018-11-14 19:30:06 -0300

    premer netfilx (si lo escribi mal)

---
+++ db/migrate/20181114222653_create_films.rb
@@ -0,0 +1,12 @@
+class CreateFilms < ActiveRecord::Migration[5.1]
+  def change
+    create_table :films do |t|
+      t.string :title
+      t.integer :year
+      t.string :description
+      t.enum :genre
+
+      t.timestamps
+    end
+  end
+end
